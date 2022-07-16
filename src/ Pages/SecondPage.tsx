5921e8376551a65bf7b220a814ba9131d1ef5d0e
Amila-siriwardhana <amilasiriwardhana660@gmail.com>
2022-07-16 12:19:47 +0530

    second page form layout

--- src/ Pages/SecondPage.tsx
+++ src/ Pages/SecondPage.tsx
@@ -15,68 +15,93 @@ const SecondPage = () => {
       <div className="col-6 p-0">
         <div className="card">
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="inputdiv p-3">
-              <input
-                type="number"
-                placeholder="Price"
-                {...register("Price", {
-                  required: true,
-                  min: 0,
-                  maxLength: 80,
-                })}
-              />
+            <div className="inputgroup">
+              <label>Price</label>
+              <div className="inputdiv ">
+                <input
+                  type="number"
+                  placeholder="Price"
+                  {...register("Price", {
+                    required: true,
+                    min: 0,
+                    maxLength: 80,
+                  })}
+                />
+              </div>
             </div>
-            <div className="inputdiv p-3">
-              <input
-                type="number"
-                placeholder="Points"
-                {...register("Points", {
-                  required: true,
-                  min: 0,
-                  maxLength: 100,
-                })}
-              />
+            <div className="inputgroup">
+              <label>Points</label>
+              <div className="inputdiv ">
+                <input
+                  type="number"
+                  placeholder="Points"
+                  {...register("Points", {
+                    required: true,
+                    min: 0,
+                    maxLength: 100,
+                  })}
+                />
+              </div>
             </div>
-            <div className="inputdiv p-3">
-              <select {...register("Possible Purchase", { required: true })}>
-                <option value="Null"> Null</option>
-                <option value="1"> 1</option>
-              </select>
+            <div className="inputgroup">
+              <label>Possible Purchase</label>
+              <div className="inputdiv ">
+                <select {...register("Possible Purchase", { required: true })}>
+                  <option value="Null"> Null</option>
+                  <option value="1"> 1</option>
+                </select>
+              </div>
             </div>
-            <div className="inputdiv p-3">
-              <input
-                type="number"
-                placeholder="Discount Amount"
-                {...register("Discount Amount", {
-                  required: true,
-                  min: 0,
-                  maxLength: 140,
-                })}
-              />
+            <div className="inputgroup">
+              <label>Discount Amount</label>
+              <div className="inputdiv ">
+                <input
+                  type="number"
+                  placeholder="Discount Amount"
+                  {...register("Discount Amount", {
+                    required: true,
+                    min: 0,
+                    maxLength: 140,
+                  })}
+                />
+              </div>
             </div>
-            <div className="inputdiv p-3">
-              <textarea
-                placeholder="Description"
-                {...register("Description", { min: 0, maxLength: 20 })}
-              />
+            <div className="inputgroup">
+              <label>Description</label>
+              <div className="inputdiv ">
+                <textarea
+                  placeholder="Description"
+                  {...register("Description", { min: 0, maxLength: 20 })}
+                />
+              </div>
             </div>
-            <div className="inputdiv p-3">
-              <textarea
-                {...register("Long Description", {
-                  required: true,
-                  min: 0,
-                  maxLength: 200,
-                })}
-              />
+            <div className="inputgroup">
+              <label>Long Description</label>
+              <div className="inputdiv ">
+                <textarea
+                  {...register("Long Description", {
+                    required: true,
+                    min: 0,
+                    maxLength: 200,
+                  })}
+                />
+              </div>
             </div>
-            <div className="inputdiv p-3">
-              <textarea
-                {...register("Extra Information", { min: 0, maxLength: 250 })}
-              />
+            <div className="inputgroup">
+              <label>Extra Information</label>
+              <div className="inputdiv">
+                <textarea
+                  {...register("Extra Information", { min: 0, maxLength: 250 })}
+                />
+              </div>
             </div>
-
-            <div className="inputdiv p-3">
-              <input type="submit" />
+            <div>
+              <button className="submit_button_benifits px-4 py-2 m-5">
+                Add Benifits
+              </button>
+              <button className="submit_button_plus px-4 py-3" type="submit">
+                +
+              </button>
             </div>
           </form>
         </div>
