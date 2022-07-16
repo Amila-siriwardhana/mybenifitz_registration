e8242aefe12dc233fb2e7f235013aedd38f833d8
sriyabro <sriyabro@gmail.com>
2022-07-16 12:49:31 +0530

    background added, conatiner changes

--- src/ Pages/SecondPage.tsx
+++ src/ Pages/SecondPage.tsx
@@ -15,55 +15,61 @@ const SecondPage = () => {
       <div className="col-6 p-0">
         <div className="card">
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="inputgroup">
-              <label>Price</label>
-              <div className="inputdiv ">
-                <input
-                  type="number"
-                  placeholder="Price"
-                  {...register("Price", {
-                    required: true,
-                    min: 0,
-                    maxLength: 80,
-                  })}
-                />
+            <div className="row">
+              <div className="inputgroup col-6 left">
+                <label>Price</label>
+                <div className="inputdiv ">
+                  <input
+                    type="number"
+                    placeholder="Price"
+                    {...register("Price", {
+                      required: true,
+                      min: 0,
+                      maxLength: 80,
+                    })}
+                  />
+                </div>
+              </div>
+              <div className="inputgroup col-6 right">
+                <label>Points</label>
+                <div className="inputdiv ">
+                  <input
+                    type="number"
+                    placeholder="Points"
+                    {...register("Points", {
+                      required: true,
+                      min: 0,
+                      maxLength: 100,
+                    })}
+                  />
+                </div>
               </div>
             </div>
-            <div className="inputgroup">
-              <label>Points</label>
-              <div className="inputdiv ">
-                <input
-                  type="number"
-                  placeholder="Points"
-                  {...register("Points", {
-                    required: true,
-                    min: 0,
-                    maxLength: 100,
-                  })}
-                />
+            <div className="row">
+              <div className="inputgroup col-6 left">
+                <label>Possible Purchase</label>
+                <div className="inputdiv ">
+                  <select
+                    {...register("Possible Purchase", { required: true })}
+                  >
+                    <option value="Null"> Null</option>
+                    <option value="1"> 1</option>
+                  </select>
+                </div>
               </div>
-            </div>
-            <div className="inputgroup">
-              <label>Possible Purchase</label>
-              <div className="inputdiv ">
-                <select {...register("Possible Purchase", { required: true })}>
-                  <option value="Null"> Null</option>
-                  <option value="1"> 1</option>
-                </select>
-              </div>
-            </div>
-            <div className="inputgroup">
-              <label>Discount Amount</label>
-              <div className="inputdiv ">
-                <input
-                  type="number"
-                  placeholder="Discount Amount"
-                  {...register("Discount Amount", {
-                    required: true,
-                    min: 0,
-                    maxLength: 140,
-                  })}
-                />
+              <div className="inputgroup col-6 right">
+                <label>Discount Amount</label>
+                <div className="inputdiv ">
+                  <input
+                    type="number"
+                    placeholder="Discount Amount"
+                    {...register("Discount Amount", {
+                      required: true,
+                      min: 0,
+                      maxLength: 140,
+                    })}
+                  />
+                </div>
               </div>
             </div>
             <div className="inputgroup">
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-
-import FirstPage from './ Pages/FirstPage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Header } from './Components/Header';
-import SecondPage from './ Pages/SecondPage';
-import ThirdPage from './ Pages/ThirdPage';
+import FirstPage from "./ Pages/FirstPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./Components/Header";
+import SecondPage from "./ Pages/SecondPage";
+import ThirdPage from "./ Pages/ThirdPage";
 
 function App() {
   return (
     <BrowserRouter>
-    <Header/>
-      <Routes>
-        <Route path="/build_loyalty_club" element={<FirstPage/>}/>    
-        <Route path="/add_business_to_club" element={<SecondPage/>}/>
-        <Route path="/define_brand" element={<ThirdPage/>}/>
-      </Routes>
-      </BrowserRouter>
-   
+      <div className="container-fluid app-conatiner">
+        <Header />
+        <Routes>
+          <Route path="/build_loyalty_club" element={<FirstPage />} />
+          <Route path="/add_business_to_club" element={<SecondPage />} />
+          <Route path="/define_brand" element={<ThirdPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
