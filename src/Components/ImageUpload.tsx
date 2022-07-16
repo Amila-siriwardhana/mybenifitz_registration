40c7f7aa48f60f929cce520bd3d4f32fdbdf3c58
sriyabro <sriyabro@gmail.com>
2022-07-16 16:23:33 +0530

    image upload changes

--- src/Components/ImageUpload.tsx
+++ src/Components/ImageUpload.tsx
@@ -1,12 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Dropzone from "react-dropzone";
-import {Trash2, Upload} from "react-feather";
+import { Trash2, Upload } from "react-feather";
 
 type ImageUploadProps = {
-  setImageFile: (file: any) => void
-}
+  setImageFile: (file: any) => void;
+};
 
-const ImageUpload: React.FC<ImageUploadProps> = ({setImageFile}) => {
+const ImageUpload: React.FC<ImageUploadProps> = ({ setImageFile }) => {
   const [fileUrl, setFileUrl] = useState<string>();
   const [imagePresent, setImagePresent] = useState<boolean>(false);
 
@@ -14,42 +14,43 @@ const ImageUpload: React.FC<ImageUploadProps> = ({setImageFile}) => {
     setImageFile(acceptedFiles[0]);
     setFileUrl(URL.createObjectURL(acceptedFiles[0]));
     setImagePresent(true);
-  }
+  };
 
   const handleImageRemove = () => {
     setImageFile(null);
     setFileUrl(undefined);
     setImagePresent(false);
-  }
+  };
 
   return (
-    <React.Fragment>
-
-      <Dropzone
-        disabled={imagePresent}
-        onDrop={handleDrop}
-        // accept="image/*"
-        minSize={1024}
-        maxSize={3072000}
-      >
-        {({getRootProps, getInputProps}) => (
-          <div {...getRootProps({className: "dropzone"})}>
-            <input {...getInputProps()} />
-            <div className="preview-image float-right">
-
-              {fileUrl === undefined ?
-                <Upload className="upload-icon" size={30}/> :
-                <>
-                  <Trash2 className="remove-icon" size={15} onClick={handleImageRemove}/>
-                  <img className="image" src={fileUrl} alt="product image"/>
-                </>
-              }
-            </div>
+    <Dropzone
+      disabled={imagePresent}
+      onDrop={handleDrop}
+      accept={{ "image/jpeg": [], "image/png": [] }}
+      minSize={1024}
+      maxSize={3072000}
+    >
+      {({ getRootProps, getInputProps }) => (
+        <div {...getRootProps({ className: "dropzone" })}>
+          <input {...getInputProps()} />
+          <div className="preview-image float-right">
+            {fileUrl === undefined ? (
+              <Upload className="upload-icon" size={30} />
+            ) : (
+              <>
+                <Trash2
+                  className="remove-icon"
+                  size={15}
+                  onClick={handleImageRemove}
+                />
+                <img className="image" src={fileUrl} alt="logo" />
+              </>
+            )}
           </div>
-        )}
-      </Dropzone>
-    </React.Fragment>
+        </div>
+      )}
+    </Dropzone>
   );
-}
+};
 
 export default ImageUpload;
