86c8689301b2f5339f0ed30f888fd314ce0d3e18
sriyabro <sriyabro@gmail.com>
2022-07-17 13:26:52 +0530

    regex changes

--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -153,7 +153,7 @@ const FirstPage = () => {
                     {...register("website", {
                       pattern: {
                         value:
-                          /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi,
+                          /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)?/gi,
                         message: "Please enter a valid website URL",
                       },
                     })}
