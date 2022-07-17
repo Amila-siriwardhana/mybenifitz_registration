a3dd2c99b206bef9138d8c98387ffde34f5b28d3
sriyabro <sriyabro@gmail.com>
2022-07-17 11:19:48 +0530

    merge error fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import FirstPage from "./pages/FirstPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Header } from "./Components/Header";
+import { Header } from "./components/Header";
 import SecondPage from "./pages/SecondPage";
 import ThirdPage from "./pages/ThirdPage";
 
